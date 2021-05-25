55a74e2571ddbb64baf6df11b88bfbfed988c5c5
B22 -user <asglazunova@gmail.com>
2021-05-25 19:23:37 -0400

    share on gitHub

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,12 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        //created new project
+        // enable version control
+        // added .gitIgnore with .idea , target folder and .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
+
+
     }
 }
